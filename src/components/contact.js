3960bf1aa4ff0d85c6971cbaaa09cce384f85b4c
kyle-t-r <100729344+kyle-t-r@users.noreply.github.com>
2023-09-05 08:12:15 -0400

    Taking the header out of the form/back to post.

--- src/components/contact.js
+++ src/components/contact.js
@@ -34,51 +34,53 @@ function Contact() {
   }
 
   return (
-    <form name="contact" id="contact" class="contact-form">
+    <div class="contact-container">
       <h2 id="contact-label" class="contact-label">
         Let's get in touch.
       </h2>
-      <input type="hidden" name="form-name" value="contact" />
-      <input
-        type="text"
-        class="contact-input"
-        name="name"
-        id="name"
-        placeholder="Name"
-        onInput={(e) => setName(e.target.value)}
-      />
+      <form name="contact" id="contact" class="contact-form" method="POST">
+        <input type="hidden" name="form-name" value="contact" />
+        <input
+          type="text"
+          class="contact-input"
+          name="name"
+          id="name"
+          placeholder="Name"
+          onInput={(e) => setName(e.target.value)}
+        />
 
-      <input
-        type="email"
-        class="contact-input"
-        name="email"
-        id="email"
-        placeholder="Email"
-        onInput={(e) => setEmail(e.target.value)}
-      />
+        <input
+          type="email"
+          class="contact-input"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onInput={(e) => setEmail(e.target.value)}
+        />
 
-      <textarea
-        class="contact-message"
-        name="message"
-        id="message"
-        cols="30"
-        rows="8"
-        placeholder="How can I help you?"
-        onInput={(e) => setMessage(e.target.value)}
-      ></textarea>
+        <textarea
+          class="contact-message"
+          name="message"
+          id="message"
+          cols="30"
+          rows="8"
+          placeholder="How can I help you?"
+          onInput={(e) => setMessage(e.target.value)}
+        ></textarea>
 
-      <button
-        type="button"
-        class="contact-submit"
-        name="btnSubmit"
-        value="Send"
-        onClick={() => {
-          Validate();
-        }}
-      >
-        Submit
-      </button>
-    </form>
+        <button
+          type="button"
+          class="contact-submit"
+          name="btnSubmit"
+          value="Send"
+          onClick={() => {
+            Validate();
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
