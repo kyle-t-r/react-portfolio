044a8f7676e3f4a63cc47a151deb69985c63b4fb
kyle-t-r <100729344+kyle-t-r@users.noreply.github.com>
2023-09-05 11:37:22 -0400

    Better tablet image spacing and hero animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,9 +2,8 @@ function Hero() {
   return (
     <div class="hero-container" id="home">
       <div class="hero-intro">
-        <h2>Hey!</h2>
         <h1>I'm Kyle Radick,</h1>
-        <h5>Full Stack Developer.</h5>
+        <h5 class="anim-typewriter">&#123;Full Stack Developer&#125;</h5>
       </div>
       <div class="hero-portrait">
         <img src="linkedin.jpg" alt="Portrait" class="hero-portrait-image" />
